3decdba8ab7bf01594929ae3d02505c33ddf30bd
cindy <322169xyu@gmail.com>
2024-04-19 02:25:59 -0700

    update on app.py

--- 23f-project-boilerplate-main/flask-app/app.py
+++ 23f-project-boilerplate-main/flask-app/app.py
@@ -3,12 +3,16 @@
 ###
 
 # import the create app function 
-# that lives in src/__init__.py
-from src import create_app
+# Create the object app
+from flask import Flask
 
-# create the app object
-app = create_app()
+# The __name__ variable is a special variable in Python that contains the name
+# of the current flask module
+app = Flask(__name__)
 
+@app.route('/')
+def index(): # The view function index() is linked to the route using the app.route decorator
+    return "This is an example "
 if __name__ == '__main__':
     # we want to run in debug mode (for hot reloading) 
     # this app will be bound to port 4000. 
