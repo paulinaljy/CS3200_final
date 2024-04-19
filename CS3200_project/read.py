7e392f4d88c1490e7ed52d716f4110c13e7674fa
Paulina Lee <paulinalee718@gmail.com>
2024-04-18 17:59:02 -0700

    read

--- CS3200_project/read.py
+++ CS3200_project/read.py
@@ -4,3 +4,9 @@ from database import mysql
 read = Blueprint('read', __name__)
 
 @read.route('/read')
+def default():
+    cursor = mysql.get_db().cursor()
+    response = cursor.execute("SELECT * FROM colleges")
+    if response > 0:
+        colleges = cursor.fetchall()
+        return render_template('index.html', list = colleges)
